255ca5d3275f8a50821513a5e89a1554784c9cea
Sushil Ashok Pote <166101754+sushilpote13@users.noreply.github.com>
2026-06-22 09:57:31 +0530

    Time: 0 ms (100%), Space: 44 MB (10.87%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        int k = -1;
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                k = i;
+                break;
+            }
+        }
+        int st, en;
+        // Array not rotated
+        if (k == -1) {
+            st = 0;
+            en = n - 1;
+        }
+        // Target in left sorted part
+        else if (target >= nums[0]) {
+            st = 0;
+            en = k;
+        }
+        // Target in right sorted part
+        else {
+            st = k + 1;
+            en = n - 1;
+        }
+
+        // Binary Search
+        while (st <= en) {
+            int mid = st + (en - st) / 2;
+
+            if (nums[mid] == target)
+                return mid;
+
+            if (nums[mid] < target)
+                st = mid + 1;
+            else
+                en = mid - 1;
+        }
+
+        return -1;
+    }
+}
