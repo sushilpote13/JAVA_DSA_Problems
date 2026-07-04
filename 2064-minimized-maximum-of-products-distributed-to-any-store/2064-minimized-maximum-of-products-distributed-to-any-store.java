03430158567fac38c2377a7c6e04adc275a602cb
Sushil Ashok Pote <166101754+sushilpote13@users.noreply.github.com>
2026-07-04 23:58:14 +0530

    Time: 115 ms (6.35%), Space: 81.1 MB (71.56%) - LeetHub

--- 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.java
+++ 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.java
@@ -2,23 +2,24 @@ class Solution {
     public int minimizedMaximum(int n, int[] quantities) {
         int high = Integer.MIN_VALUE;
         for (int i : quantities) {
-            if (high < i)
+            if (high < i) {
                 high = i;
+            }
         }
-        int ans = Integer.MAX_VALUE, m = quantities.length, low = 1;
+        int low = 1;
+        int mid = low + ((high - low) / 2);
         while (low <= high) {
-            int mid = low + (high - low) / 2;
             int count = 0;
-            for (int q : quantities) {
-                count += (q + mid - 1) / mid; // integer ceil
+            for (int i : quantities) {
+                count += Math.ceil(i / (double) mid);
             }
-            if (count <= n) {
-                ans = mid;
-                high = mid - 1;
-            } else {
+            if (count > n) {
                 low = mid + 1;
+            } else {
+                high = mid - 1;
             }
+            mid = low + ((high - low) / 2);
         }
-        return ans;
+        return mid;
     }
 }
