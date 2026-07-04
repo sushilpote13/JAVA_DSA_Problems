1a1108ba5e603501a4afe4bf152ffe4f40e49ed5
Sushil Ashok Pote <166101754+sushilpote13@users.noreply.github.com>
2026-07-05 00:39:02 +0530

    Time: 1 ms (100%), Space: 48.5 MB (93.5%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,44 +1,31 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n = nums1.length, m = nums2.length;
+        int total = n + m;
+        int median = total / 2;
 
-        int n1 = nums1.length;
-        int n2 = nums2.length;
-        int total = n1 + n2;
+        int pointerL = 0, pointerR = 0;
+        int prev = 0, curr = 0;
 
-        int mid1 = (total - 1) / 2;
-        int mid2 = total / 2;
+        for (int i = 0; i <= median; i++) {
 
-        int i = 0, j = 0;
-        int index = 0;
+            prev = curr;
 
-        int first = 0, second = 0;
-
-        while (i < n1 || j < n2) {
-
-            int val;
-
-            if (i < n1 && (j >= n2 || nums1[i] <= nums2[j])) {
-                val = nums1[i];
-                i++;
+            if (pointerL >= n) {
+                curr = nums2[pointerR++];
+            } else if (pointerR >= m) {
+                curr = nums1[pointerL++];
+            } else if (nums1[pointerL] <= nums2[pointerR]) {
+                curr = nums1[pointerL++];
             } else {
-                val = nums2[j];
-                j++;
+                curr = nums2[pointerR++];
             }
-
-            if (index == mid1)
-                first = val;
-
-            if (index == mid2) {
-                second = val;
-                break;
-            }
-
-            index++;
         }
 
-        if (total % 2 == 1)
-            return second;
+        if (total % 2 == 0) {
+            return (prev + curr) / 2.0;
+        }
 
-        return (first + second) / 2.0;
+        return curr;
     }
 }
