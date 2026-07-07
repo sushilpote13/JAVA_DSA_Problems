d04f73c0c3b1a80cfc1ff612373f1227123f6971
Sushil Ashok Pote <166101754+sushilpote13@users.noreply.github.com>
2026-07-08 01:11:24 +0530

    Time: 2 ms (99.7%), Space: 45.1 MB (60.36%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,25 +1,27 @@
 class Solution {
-    public static void backtrack(int[] nums, int index, List<Integer> subset, List<List<Integer>> res) {
-        if (index == nums.length) {
-            res.add(new ArrayList<>(subset));
-            return;
+
+    public void backtrack(int[] nums, int start, List<Integer> subset, List<List<Integer>> res) {
+        res.add(new ArrayList<>(subset));
+
+        for (int i = start; i < nums.length; i++) {
+
+            // Skip duplicates
+            if (i > start && nums[i] == nums[i - 1])
+                continue;
+
+            subset.add(nums[i]);
+            backtrack(nums, i + 1, subset, res);
+            subset.remove(subset.size() - 1);
         }
-        // all subset that include nums[i]
-        subset.add(nums[index]);
-        backtrack(nums, index + 1, subset, res);
-        subset.remove(subset.size() - 1);
-        // all subsets that don't include nums[i]
-        while ((index + 1 < nums.length) && nums[index] == nums[index + 1]) {
-            index += 1;
-        }
-        backtrack(nums, index + 1, subset, res);
     }
 
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        List<Integer> list = new ArrayList<>();
+
         Arrays.sort(nums);
-        backtrack(nums, 0, list, res);
+
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(nums, 0, new ArrayList<>(), res);
+
         return res;
     }
 }
