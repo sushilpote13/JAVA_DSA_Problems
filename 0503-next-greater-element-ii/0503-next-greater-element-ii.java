0c8956f6dec6297c305e48443da44f83671c4568
Sushil Ashok Pote <166101754+sushilpote13@users.noreply.github.com>
2026-07-19 08:36:12 +0530

    Time: 15 ms (61.88%), Space: 48.5 MB (60.07%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -3,11 +3,11 @@ import java.util.*;
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
         int n = nums.length;
+        int[] ans = new int[n];
 
-        Stack<Integer> st = new Stack<>();      // Monotonic stack
-        Stack<Integer> ansStack = new Stack<>(); // Stores answers
+        Stack<Integer> st = new Stack<>();
 
-        // Traverse twice because array is circular
+        // Traverse twice
         for (int i = 2 * n - 1; i >= 0; i--) {
 
             while (!st.isEmpty() && st.peek() <= nums[i % n]) {
@@ -15,20 +15,10 @@ class Solution {
             }
 
             if (i < n) {
-                if (st.isEmpty()) {
-                    ansStack.push(-1);
-                } else {
-                    ansStack.push(st.peek());
-                }
+                ans[i] = st.isEmpty() ? -1 : st.peek();
             }
 
             st.push(nums[i % n]);
-        }
-
-        int[] ans = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            ans[i] = ansStack.pop();
         }
 
         return ans;
