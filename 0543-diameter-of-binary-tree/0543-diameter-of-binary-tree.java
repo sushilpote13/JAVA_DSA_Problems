a5995f6383ad5ec32fb116c0ce93323550831aa4
Sushil Ashok Pote <166101754+sushilpote13@users.noreply.github.com>
2026-07-26 17:57:08 +0530

    Time: 1 ms (46.04%), Space: 46.8 MB (88.63%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -1,37 +1,39 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    static int diameter = 0;
 
-    public static int height(TreeNode root) {
-        if (root == null)
-            return 0;
+    class Info {
+        int height;
+        int diameter;
 
-        int leftHeight = height(root.left);
-        int rightHeight = height(root.right);
+        Info(int height, int diameter) {
+            this.height = height;
+            this.diameter = diameter;
+        }
+    }
 
-        // Update diameter
-        diameter = Math.max(diameter, leftHeight + rightHeight);
+    public Info diameter(TreeNode root) {
 
-        return Math.max(leftHeight, rightHeight) + 1;
+        if (root == null) {
+            return new Info(0, 0);
+        }
+
+        Info left = diameter(root.left);
+        Info right = diameter(root.right);
+
+        int leftH = left.height;
+        int rightH = right.height;
+
+        int leftD = left.diameter;
+        int rightD = right.diameter;
+
+        int selfHeight = Math.max(leftH, rightH) + 1;
+        int selfDiameter = leftH + rightH;
+
+        int finalDiameter = Math.max(selfDiameter, Math.max(leftD, rightD));
+
+        return new Info(selfHeight, finalDiameter);
     }
 
     public int diameterOfBinaryTree(TreeNode root) {
-        diameter = 0;
-        height(root);
-        return diameter;
+        return diameter(root).diameter;
     }
 }
