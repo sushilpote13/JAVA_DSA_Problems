c0247ec9506767487453572c4160c3dbe1c477b0
Sushil Ashok Pote <166101754+sushilpote13@users.noreply.github.com>
2026-07-28 15:53:06 +0530

    Time: 91 ms (6.12%), Space: 47.5 MB (6.57%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -1,50 +1,42 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+import java.util.HashMap;
+
 class Solution {
-    public static void insertNode(int val, int inorderIndex, HashMap<Integer, Integer> map, TreeNode root) {
-        // base condition if root is null
+
+    public static void insertNode(int val, int inorderIndex,
+                                  HashMap<Integer, Integer> map,
+                                  TreeNode root) {
+
         if (root == null) {
             return;
         }
 
-        // when to go to left
         if (inorderIndex < map.get(root.val)) {
+
             if (root.left == null) {
                 root.left = new TreeNode(val);
             } else {
                 insertNode(val, inorderIndex, map, root.left);
             }
+
         } else {
+
             if (root.right == null) {
                 root.right = new TreeNode(val);
             } else {
                 insertNode(val, inorderIndex, map, root.right);
             }
+
         }
     }
 
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        // create the HashMap for getting the indexes
+
         HashMap<Integer, Integer> map = new HashMap<>();
 
         for (int i = 0; i < inorder.length; i++) {
             map.put(inorder[i], i);
         }
 
-        // insert the first element
         TreeNode root = new TreeNode(preorder[0]);
 
         for (int i = 1; i < preorder.length; i++) {
