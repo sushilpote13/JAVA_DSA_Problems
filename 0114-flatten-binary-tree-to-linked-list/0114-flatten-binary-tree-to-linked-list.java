8da0c71af79e352f8ff74b318d187c56c05395d9
Sushil Ashok Pote <166101754+sushilpote13@users.noreply.github.com>
2026-07-30 15:57:17 +0530

    Time: 0 ms (100%), Space: 43.8 MB (72.02%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -15,32 +15,19 @@
  */
 
 class Solution {
-
     public void flatten(TreeNode root) {
 
         if (root == null)
             return;
-
-        // Flatten left subtree
         flatten(root.left);
-
-        // Flatten right subtree
         flatten(root.right);
-
-        // Store original right subtree
         TreeNode originalRight = root.right;
-
-        // Move left subtree to right
         root.right = root.left;
         root.left = null;
-
-        // Find last node of new right subtree
         TreeNode curr = root;
         while (curr.right != null) {
             curr = curr.right;
         }
-
-        // Attach original right subtree
         curr.right = originalRight;
     }
 }
