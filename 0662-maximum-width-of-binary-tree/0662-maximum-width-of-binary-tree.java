b4720e532a6d8f3895c46e364dea284d2a7e2c2d
Sushil Ashok Pote <166101754+sushilpote13@users.noreply.github.com>
2026-07-30 22:27:11 +0530

    Time: 1 ms (99.71%), Space: 44.6 MB (98.3%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
@@ -13,51 +13,25 @@
  *     }
  * }
  */
-
 class Solution {
-
-    class Pair {
-        TreeNode node;
-        long index;
-
-        Pair(TreeNode node, long index) {
-            this.node = node;
-            this.index = index;
-        }
-    }
-
-    public int widthOfBinaryTree(TreeNode root) {
-        if (root == null) {
+    public int bfs(TreeNode root,int level,int id,ArrayList<Integer> list){
+        if(root==null){
             return 0;
         }
-        Queue<Pair> queue = new LinkedList<>();
-        queue.offer(new Pair(root, 0));
-        int maxWidth = 0;
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            long minIndex = queue.peek().index;
-            long first = 0;
-            long last = 0;
-            for (int i = 0; i < size; i++) {
-                Pair current = queue.poll();
-                TreeNode node = current.node;
-                // Normalize the index
-                long currentIndex = current.index - minIndex;
-                if (i == 0) {
-                    first = currentIndex;
-                }
-                if (i == size - 1) {
-                    last = currentIndex;
-                }
-                if (node.left != null) {
-                    queue.offer(new Pair(node.left, 2 * currentIndex + 1));
-                }
-                if (node.right != null) {
-                    queue.offer(new Pair(node.right, 2 * currentIndex + 2));
-                }
-            }
-            maxWidth = Math.max(maxWidth, (int) (last - first + 1));
+
+        if(level==list.size()){
+            list.add(id);
         }
-        return maxWidth;
+
+        int curr=id-list.get(level)+1;
+        int left=bfs(root.left,level+1,2*id+1,list);
+        int right=bfs(root.right,level+1,2*id+2,list);
+
+        return Math.max(curr,Math.max(left,right));
     }
+    public int widthOfBinaryTree(TreeNode root) {
+        ArrayList<Integer> list=new ArrayList<>();
+        return bfs(root,0,1,list);
+    }
+
 }
