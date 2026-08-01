7f820dfb5967e2740e0e1b3d4d0fc8baf4135bb9
Sushil Ashok Pote <166101754+sushilpote13@users.noreply.github.com>
2026-08-01 23:07:23 +0530

    Time: 1684 ms (5.09%), Space: 53.6 MB (6.27%) - LeetHub

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.java
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.java
@@ -1,32 +1,65 @@
-import java.util.PriorityQueue;
-
 class KthLargest {
 
-    private PriorityQueue<Integer> minHeap;
+    class TreeNode {
+        int val;
+        int count;   // Number of duplicates
+        int size;    // Total nodes in subtree
+        TreeNode left, right;
+
+        TreeNode(int val) {
+            this.val = val;
+            this.count = 1;
+            this.size = 1;
+        }
+    }
+
+    private TreeNode root;
     private int k;
 
     public KthLargest(int k, int[] nums) {
         this.k = k;
-        minHeap = new PriorityQueue<>();
-
         for (int num : nums) {
-            add(num);
+            root = insert(root, num);
         }
+    }
+
+    private TreeNode insert(TreeNode node, int val) {
+        if (node == null) {
+            return new TreeNode(val);
+        }
+
+        if (val < node.val) {
+            node.left = insert(node.left, val);
+        } else if (val > node.val) {
+            node.right = insert(node.right, val);
+        } else {
+            node.count++;
+        }
+
+        node.size = size(node.left) + size(node.right) + node.count;
+        return node;
+    }
+
+    private int size(TreeNode node) {
+        return node == null ? 0 : node.size;
+    }
+
+    private int kthLargest(TreeNode node, int k) {
+        int rightSize = size(node.right);
+
+        if (k <= rightSize) {
+            return kthLargest(node.right, k);
+        }
+
+        if (k <= rightSize + node.count) {
+            return node.val;
+        }
+
+        return kthLargest(node.left, k - rightSize - node.count);
     }
 
     public int add(int val) {
-        minHeap.offer(val);
-
-        if (minHeap.size() > k) {
-            minHeap.poll();
-        }
-
-        return minHeap.peek();
+        root = insert(root, val);
+        return kthLargest(root, k);
     }
 }
-
-/**
- * Your KthLargest object will be instantiated and called as such:
- * KthLargest obj = new KthLargest(k, nums);
- * int param_1 = obj.add(val);
- */
