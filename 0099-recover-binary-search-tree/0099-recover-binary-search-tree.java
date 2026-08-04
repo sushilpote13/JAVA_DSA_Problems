5c107d24dea8022530f5ecb11ff053542761a02f
Sushil Ashok Pote <166101754+sushilpote13@users.noreply.github.com>
2026-08-04 23:02:23 +0530

    Time: 1 ms (100%), Space: 46.5 MB (49.87%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.java
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.java
@@ -1,34 +1,36 @@
 class Solution {
 
-    public void createInorder(TreeNode root, ArrayList<Integer> list) {
-        if (root == null) return;
+    TreeNode first = null;
+    TreeNode second = null;
+    TreeNode prev = null;
 
-        createInorder(root.left, list);
-        list.add(root.val);
-        createInorder(root.right, list);
-    }
+    public void inorder(TreeNode root) {
 
-    public void placeValue(TreeNode root, ArrayList<Integer> list, int[] idx) {
-        if (root == null) return;
+        if (root == null)
+            return;
 
-        placeValue(root.left, list, idx);
+        inorder(root.left);
 
-        root.val = list.get(idx[0]);
-        idx[0]++;
+        if (prev != null && prev.val > root.val) {
 
-        placeValue(root.right, list, idx);
+            if (first == null) {
+                first = prev;
+            }
+
+            second = root;
+        }
+
+        prev = root;
+
+        inorder(root.right);
     }
 
     public void recoverTree(TreeNode root) {
 
-        ArrayList<Integer> inorder = new ArrayList<>();
+        inorder(root);
 
-        createInorder(root, inorder);
-
-        Collections.sort(inorder);
-
-        int[] idx = {0};
-
-        placeValue(root, inorder, idx);
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
     }
 }
