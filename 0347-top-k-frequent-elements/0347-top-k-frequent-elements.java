f81763bd5c2f71c4a9bfb6b36889fc4c34d95253
Sushil Ashok Pote <166101754+sushilpote13@users.noreply.github.com>
2026-08-07 21:09:05 +0530

    Time: 14 ms (70.88%), Space: 47.6 MB (49.74%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,33 +1,33 @@
-import java.util.*;
-
 class Solution {
+    class Pair {
+        int num;
+        int freq;
+
+        Pair(int num, int freq) {
+            this.num = num;
+            this.freq = freq;
+        }
+    }
+
     public int[] topKFrequent(int[] nums, int k) {
-
-        // Step 1: Count frequency
+        // count the frequency of each element and stor it in the hashmap
         HashMap<Integer, Integer> map = new HashMap<>();
-
         for (int num : nums) {
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
-
-        // Step 2: Min Heap based on frequency
-        PriorityQueue<Map.Entry<Integer, Integer>> minHeap =
-                new PriorityQueue<>((a, b) -> a.getValue() - b.getValue());
-
-        // Step 3: Keep only k most frequent elements
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            minHeap.offer(entry);
-
+        // create a Priority Queue on the hashmap
+        PriorityQueue<Pair> minHeap = new PriorityQueue<>((a, b) -> a.freq - b.freq);
+        for (int key : map.keySet()) {
+            minHeap.offer(new Pair(key, map.get(key)));
             if (minHeap.size() > k) {
                 minHeap.poll();
             }
         }
-
-        // Step 4: Store answer
+        // create the int[] and append all the values in that array 
         int[] ans = new int[k];
 
         for (int i = k - 1; i >= 0; i--) {
-            ans[i] = minHeap.poll().getKey();
+            ans[i] = minHeap.poll().num;
         }
 
         return ans;
