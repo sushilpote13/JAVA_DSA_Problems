0a85467768c11d3daea4142cf491ef816d2360c0
Sushil Ashok Pote <166101754+sushilpote13@users.noreply.github.com>
2026-08-08 14:38:52 +0530

    Time: 1 ms (61.27%), Space: 43.3 MB (32.74%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,19 +1,49 @@
 class Solution {
+    class Node {
+        Node[] children = new Node[26];
+        boolean eow = false;
+    }
+    Node root = new Node();
+    // Insert a word into Trie
+    public void insert(String word) {
+        Node curr = root;
+        for (int i = 0; i < word.length(); i++) {
+            int idx = word.charAt(i) - 'a';
+            if (curr.children[idx] == null) {
+                curr.children[idx] = new Node();
+            }
+            curr = curr.children[idx];
+        }
+        curr.eow = true;
+    }
     public String longestCommonPrefix(String[] strs) {
-        String ans = strs[0];
-        int n = strs.length;
-        for (int i = 1; i < n; i++) {
-            int cured = 0;
-            while (cured < ans.length() && cured < strs[i].length()) {
-                if (ans.charAt(cured) == strs[i].charAt(cured)) {
-                    cured++;
-                } else {
-                    break;
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        // Build Trie
+        for (String str : strs) {
+            insert(str);
+        }
+        StringBuilder ans = new StringBuilder();
+        Node curr = root;
+        while (!curr.eow) {
+            int count = 0;
+            int nextIndex = -1;
+            // Count children
+            for (int i = 0; i < 26; i++) {
+                if (curr.children[i] != null) {
+                    count++;
+                    nextIndex = i;
                 }
             }
-
-            ans = ans.substring(0, cured);
+            // More than one child -> prefix ends
+            if (count != 1) {
+                break;
+            }
+            // Move to the only child
+            ans.append((char) ('a' + nextIndex));
+            curr = curr.children[nextIndex];
         }
-        return ans;
+        return ans.toString();
     }
 }
