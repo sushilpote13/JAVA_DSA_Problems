f253456cb2896ad7b2c08de49021f47caa2e337f
Sushil Ashok Pote <166101754+sushilpote13@users.noreply.github.com>
2026-08-10 09:18:07 +0530

    Time: 3 ms (99.77%), Space: 46.3 MB (99.48%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -10,54 +10,42 @@ class Solution {
     }
 
     public boolean hasCycle(int curr, ArrayList<Edge>[] graph, boolean[] vis, boolean[] stack) {
-        // mark the curr course visted
         vis[curr] = true;
         stack[curr] = true;
-
-        // vist all the neighbore of curr
         for (int i = 0; i < graph[curr].size(); i++) {
-            // if the node is visted or not 
             Edge e = graph[curr].get(i);
-
-            //case 1. if it is not visted 
+            // Node not visited
             if (!vis[e.dest]) {
-                // check cycle for that neighbore
                 if (hasCycle(e.dest, graph, vis, stack)) {
                     return true;
                 }
             }
-
-            // case 2. if it is visted then check in the current DFS
-            if (stack[e.dest]) {
+            // Node already visited and still in current DFS path
+            else if (stack[e.dest]) {
                 return true;
             }
         }
-        // we are done exploring this node remove curr from the stack 
+        // Remove from current DFS path
         stack[curr] = false;
         return false;
     }
 
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        // build graph first
+        // Build graph
         ArrayList<Edge>[] graph = new ArrayList[numCourses];
-
-        // create an ArrayList for every courses;
         for (int i = 0; i < numCourses; i++) {
             graph[i] = new ArrayList<>();
         }
-
-        // build graph
         for (int[] p : prerequisites) {
             int course = p[0];
             int prerequisite = p[1];
+
             // prerequisite -> course
-
-            graph[prerequisite].add(new Edge(prerequisite, course));
+            graph[prerequisite].add(
+                    new Edge(prerequisite, course));
         }
-        // Visited array
-        boolean[] vis = new boolean[numCourses];
 
-        // Current DFS path
+        boolean[] vis = new boolean[numCourses];
         boolean[] stack = new boolean[numCourses];
 
         for (int i = 0; i < numCourses; i++) {
@@ -65,7 +53,6 @@ class Solution {
                 if (hasCycle(i, graph, vis, stack)) {
                     return false;
                 }
-
             }
         }
         return true;
