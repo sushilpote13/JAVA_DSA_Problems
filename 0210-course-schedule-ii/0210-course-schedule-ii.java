6dbc231e7423fc2f7f70b9536e1ba3b890e41c85
Sushil Ashok Pote <166101754+sushilpote13@users.noreply.github.com>
2026-08-15 09:03:40 +0530

    Time: 6 ms (42.45%), Space: 48.5 MB (6.82%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -11,61 +11,46 @@ class Solution {
 
     public int[] findOrder(int numCourses, int[][] prerequisites) {
 
-        // create graph first
         ArrayList<Edges>[] graph = new ArrayList[numCourses];
-
         for (int i = 0; i < graph.length; i++) {
             graph[i] = new ArrayList<>();
         }
 
-        // fill the graph
         for (int[] edge : prerequisites) {
             graph[edge[1]].add(new Edges(edge[1], edge[0]));
         }
 
-        // calculate indegree
         int[] indegree = new int[numCourses];
-
         for (int i = 0; i < graph.length; i++) {
             for (Edges e : graph[i]) {
                 indegree[e.dest]++;
             }
         }
 
-        // add indegree 0 courses in queue
         Queue<Integer> q = new LinkedList<>();
-
         for (int i = 0; i < numCourses; i++) {
             if (indegree[i] == 0) {
                 q.add(i);
             }
         }
 
-        // topological sort
         int[] result = new int[numCourses];
         int index = 0;
-
         while (!q.isEmpty()) {
-
             int cur = q.remove();
-
             result[index++] = cur;
-
             for (Edges e : graph[cur]) {
-
                 indegree[e.dest]--;
-
                 if (indegree[e.dest] == 0) {
                     q.add(e.dest);
                 }
             }
         }
 
-        // cycle detected
         if (index != numCourses) {
             return new int[0];
         }
-
+        
         return result;
     }
 }
