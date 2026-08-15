716a1f7258705ad03f2d04eafc2e19949d540b17
Sushil Ashok Pote <166101754+sushilpote13@users.noreply.github.com>
2026-08-15 09:26:05 +0530

    Time: 4 ms (93.95%), Space: 46.9 MB (38.53%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,16 +1,32 @@
 class Solution {
+    public class Edges {
+        int src;
+        int dest;
+
+        public Edges(int src, int dest) {
+            this.src = src;
+            this.dest = dest;
+        }
+    }
+
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        int[][] adj = new int[numCourses][numCourses];
-        for (int[] edges : prerequisites) {
-            adj[edges[1]][edges[0]]++;
+        ArrayList<Edges>[] graph = new ArrayList[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            graph[i] = new ArrayList<>();
         }
 
         int[] indegree = new int[numCourses];
-        for (int[] edges : prerequisites) {
-            indegree[edges[0]]++;
+        for (int[] edge : prerequisites) {
+            int prerequisite = edge[1];
+            int course = edge[0];
+
+            graph[prerequisite].add(
+                new Edges(prerequisite, course)
+            );
+            indegree[course]++;
         }
 
-        Queue<Integer> q = new LinkedList<>();
+        Queue<Integer> q = new ArrayDeque<>();
         for (int i = 0; i < numCourses; i++) {
             if (indegree[i] == 0) {
                 q.add(i);
@@ -19,15 +35,15 @@ class Solution {
 
         int[] result = new int[numCourses];
         int index = 0;
+
+        // Kahn's Algorithm
         while (!q.isEmpty()) {
             int cur = q.remove();
             result[index++] = cur;
-            for (int i = 0; i < adj[cur].length; i++) {
-                if (adj[cur][i] == 1) {
-                    indegree[i]--;
-                    if (indegree[i] == 0) {
-                        q.add(i);
-                    }
+            for (Edges e : graph[cur]) {
+                indegree[e.dest]--;
+                if (indegree[e.dest] == 0) {
+                    q.add(e.dest);
                 }
             }
         }
