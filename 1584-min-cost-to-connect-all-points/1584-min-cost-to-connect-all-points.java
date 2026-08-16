3adf510f3d09bac807e31188fd519abcfe253cbc
Sushil Ashok Pote <166101754+sushilpote13@users.noreply.github.com>
2026-08-16 10:41:36 +0530

    Time: 673 ms (10.89%), Space: 173 MB (6.81%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -1,7 +1,6 @@
 import java.util.*;
 
 class Solution {
-
     public class Edges {
         int src;
         int dest;
@@ -30,74 +29,41 @@ class Solution {
     }
 
     public int minCostConnectPoints(int[][] points) {
-
         int n = points.length;
-
-        // -------------------------
         // STEP 1: Build Graph
-        // -------------------------
-
         ArrayList<Edges>[] graph = new ArrayList[n];
-
         for (int i = 0; i < n; i++) {
             graph[i] = new ArrayList<>();
         }
-
         for (int i = 0; i < n; i++) {
-
             for (int j = i + 1; j < n; j++) {
-
                 int wt = Math.abs(points[i][0] - points[j][0])
-                       + Math.abs(points[i][1] - points[j][1]);
-
+                        + Math.abs(points[i][1] - points[j][1]);
                 graph[i].add(new Edges(i, j, wt));
                 graph[j].add(new Edges(j, i, wt));
             }
         }
-
-        // -------------------------
         // STEP 2: Prim's Algorithm
-        // -------------------------
-
         boolean[] visited = new boolean[n];
-
         PriorityQueue<Info> pq = new PriorityQueue<>();
-
-        // Start from node 0
         pq.add(new Info(0, 0));
-
         int totalCost = 0;
-
         while (!pq.isEmpty()) {
-
             Info curr = pq.remove();
-
             int u = curr.v;
             int cost = curr.cost;
-
-            // Already included in MST
             if (visited[u]) {
                 continue;
             }
-
-            // Include this vertex
             visited[u] = true;
-
-            // Add edge cost
             totalCost += cost;
-
-            // Visit neighbors
             for (Edges edge : graph[u]) {
-
                 int v = edge.dest;
-
                 if (!visited[v]) {
-
                     pq.add(new Info(v, edge.wt));
                 }
             }
         }
-
         return totalCost;
     }
 }
