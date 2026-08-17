51efe9ed1213133142f4fc1fd0d59469f4afd9bd
Sushil Ashok Pote <166101754+sushilpote13@users.noreply.github.com>
2026-08-17 16:38:52 +0530

    Time: 123 ms (55.56%), Space: 174 MB (6.08%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -1,36 +1,63 @@
 class Solution {
-    public int minCostConnectPoints(int[][] points) {
-        int n = points.length;
-
-        int[] dist = new int[n];
-        boolean[] visited = new boolean[n];
-
-        for (int i = 0; i < n; i++) {
-            dist[i] = Integer.MAX_VALUE;
+    public class Pair implements Comparable<Pair> {
+        int val;
+        int dist;
+        public Pair(int val, int dist) {
+            this.val = val;
+            this.dist = dist;
         }
-        dist[0] = 0;
+        @Override
+        public int compareTo(Pair p2) {
+            return this.dist - p2.dist;
+        }
+    }
+    public class Edge {
+        int src;
+        int dest;
+        int dist;
+        public Edge(int src, int dest, int dist) {
+            this.src = src;
+            this.dest = dest;
+            this.dist = dist;
+        }
+    }
+    public int minCostConnectPoints(int[][] points) {
+        int v = points.length;
+        ArrayList<Edge>[] graph = new ArrayList[v];
+        for (int i = 0; i < v; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < v; i++) {
 
-        int totalCost = 0;
-        for (int i = 0; i < n; i++) {
-            // find minimum unvisted array
-            int u = -1;
-            for (int j = 0; j < n; j++) {
-                if (!visited[j] && (u == -1 || dist[j] < dist[u])) {
-                    u = j;
-                }
+            for (int j = i + 1; j < v; j++) {
+
+                int dist =
+                    Math.abs(points[i][0] - points[j][0])
+                    + Math.abs(points[i][1] - points[j][1]);
+
+                graph[i].add(new Edge(i, j, dist));
+                graph[j].add(new Edge(j, i, dist));
             }
+        }
 
-            // vist the point
-            visited[u] = true;
-            totalCost += dist[u];
-
-            // update all the unvisted vertices 
-            for (int v = 0; v < n; v++) {
-                if (!visited[v]) {
-                    int wt = Math.abs(points[u][0] - points[v][0]) + Math.abs(points[u][1] - points[v][1]);
-                    if (wt < dist[v]) {
-                        dist[v] = wt;
-                    }
+        boolean[] visited = new boolean[v];
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        pq.add(new Pair(0, 0));
+        int totalCost = 0;
+        int count = 0;
+        while (!pq.isEmpty() && count < v) {
+            Pair curr = pq.remove();
+            int vertex = curr.val;
+            int cost = curr.dist;
+            if (visited[vertex]) {
+                continue;
+            }
+            visited[vertex] = true;
+            totalCost += cost;
+            count++;
+            for (Edge e : graph[vertex]) {
+                if (!visited[e.dest]) {
+                    pq.add(new Pair(e.dest, e.dist));
                 }
             }
         }
