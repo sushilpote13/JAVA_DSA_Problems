ba9d29da653fce0ce9d71cfeb3ab80e0b2cfaba6
Sushil Ashok Pote <166101754+sushilpote13@users.noreply.github.com>
2026-08-21 22:31:49 +0530

    Time: 24 ms (94.46%), Space: 43.8 MB (98.49%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -1,29 +1,46 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
 class Solution {
     public Node cloneGraph(Node node) {
+        HashMap<Node, Node> map = new HashMap<>();
         if (node == null) {
             return null;
         }
-
-        Map<Node, Node> map = new HashMap<>();
-
-        return dfs(node, map);
-    }
-
-    private Node dfs(Node node, Map<Node, Node> map) {
-        // Already cloned
-        if (map.containsKey(node)) {
-            return map.get(node);
-        }
-
-        // Create clone
         Node clone = new Node(node.val);
         map.put(node, clone);
-
-        // Clone all neighbors
-        for (Node neighbor : node.neighbors) {
-            clone.neighbors.add(dfs(neighbor, map));
+        // for keeping the trak of the node
+        Stack<Node> stack = new Stack<>();
+        stack.push(node);
+        while (!stack.isEmpty()) {
+            Node curr = stack.pop();
+            for (Node neighbores : curr.neighbors) {
+                // check for it is already visted or not
+                if (!map.containsKey(neighbores)) {
+                    Node neighboreClone = new Node(neighbores.val);
+                    map.put(neighbores, neighboreClone);
+                    stack.push(neighbores);
+                }
+                map.get(curr).neighbors.add(map.get(neighbores));
+            }
         }
-
         return clone;
     }
 }
