7828bdd8a0252ef3da991e1c5e627e3cc438c1fb
Sushil Ashok Pote <166101754+sushilpote13@users.noreply.github.com>
2026-08-24 23:40:07 +0530

    Time: 49 ms (11.02%), Space: 46.7 MB (79.12%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
@@ -1,74 +1,30 @@
-public class Solution {
-
-    public class Edges {
-        int from;
-        int to;
-
-        public Edges(int from, int to) {
-            this.from = from;
-            this.to = to;
-        }
-    }
-
+class Solution {
     public int removeStones(int[][] stones) {
         int n = stones.length;
-
-        ArrayList<Edges>[] graph = new ArrayList[n];
-
-        for (int i = 0; i < n; i++) {
-            graph[i] = new ArrayList<>();
-        }
-
-        // Connect stones with same x or same y
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-
-                if (stones[i][0] == stones[j][0] ||
-                    stones[i][1] == stones[j][1]) {
-
-                    graph[i].add(new Edges(i, j));
-                    graph[j].add(new Edges(j, i));
-                }
-            }
-        }
-
         boolean[] visited = new boolean[n];
-        int removed = 0;
+        int components = 0;
 
         for (int i = 0; i < n; i++) {
-
             if (!visited[i]) {
-
-                // DFS returns size of this component
-                int size = dfs(i, graph, visited);
-
-                // Keep one root, remove all other nodes
-                removed += size - 1;
+                components++;
+                dfs(i, stones, visited);
             }
         }
 
-        return removed;
+        return n - components;
     }
 
-    private int dfs(
-        int node,
-        ArrayList<Edges>[] graph,
-        boolean[] visited
-    ) {
+    void dfs(int i, int[][] stones, boolean[] visited) {
+        visited[i] = true;
 
-        visited[node] = true;
+        for (int j = 0; j < stones.length; j++) {
+            // Connect if same row or same column
+            if (!visited[j] &&
+                (stones[i][0] == stones[j][0] ||
+                 stones[i][1] == stones[j][1])) {
 
-        int count = 1;
-
-        for (Edges edge : graph[node]) {
-
-            int neighbor = edge.to;
-
-            if (!visited[neighbor]) {
-                count += dfs(neighbor, graph, visited);
+                dfs(j, stones, visited);
             }
         }
-
-        return count;
     }
 }
