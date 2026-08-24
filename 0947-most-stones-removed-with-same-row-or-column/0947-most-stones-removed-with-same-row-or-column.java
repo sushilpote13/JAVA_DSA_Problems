d4445ae462a283c0193c056cbb251fabc0508872
Sushil Ashok Pote <166101754+sushilpote13@users.noreply.github.com>
2026-08-24 23:49:26 +0530

    Time: 4 ms (98.66%), Space: 47 MB (42.59%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
@@ -1,30 +1,50 @@
 class Solution {
     public int removeStones(int[][] stones) {
-        int n = stones.length;
-        boolean[] visited = new boolean[n];
+        int[] parent = new int[20002];
+        boolean[] used = new boolean[20002];
 
-        int groups = 0;
+        for (int i = 0; i < parent.length; i++)
+            parent[i] = i;
 
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                groups++;
-                dfs(i, stones, visited);
+        int components = 0;
+
+        for (int[] s : stones) {
+            int r = s[0];
+            int c = s[1] + 10001;
+
+            if (!used[r]) {
+                used[r] = true;
+                components++;
             }
+
+            if (!used[c]) {
+                used[c] = true;
+                components++;
+            }
+
+            if (union(parent, r, c))
+                components--;
         }
 
-        return n - groups;
+        return stones.length - components;
     }
 
-    private void dfs(int i, int[][] stones, boolean[] visited) {
-        visited[i] = true;
-
-        for (int j = 0; j < stones.length; j++) {
-            if (!visited[j] &&
-                (stones[i][0] == stones[j][0] ||
-                 stones[i][1] == stones[j][1])) {
-
-                dfs(j, stones, visited);
-            }
+    private int find(int[] p, int x) {
+        while (p[x] != x) {
+            p[x] = p[p[x]];
+            x = p[x];
         }
+        return x;
+    }
+
+    private boolean union(int[] p, int a, int b) {
+        a = find(p, a);
+        b = find(p, b);
+
+        if (a == b)
+            return false;
+
+        p[b] = a;
+        return true;
     }
 }
