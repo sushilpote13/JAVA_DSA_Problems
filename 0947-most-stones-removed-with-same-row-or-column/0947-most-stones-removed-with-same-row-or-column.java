9b958356d82cc84675f785b34243d38c64e3df5a
Sushil Ashok Pote <166101754+sushilpote13@users.noreply.github.com>
2026-08-24 23:43:30 +0530

    Time: 23 ms (54.63%), Space: 46.4 MB (91.82%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
@@ -1,30 +1,46 @@
 class Solution {
     public int removeStones(int[][] stones) {
         int n = stones.length;
-        boolean[] visited = new boolean[n];
-        int components = 0;
+
+        int[] parent = new int[n];
+        int components = n;
 
         for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                components++;
-                dfs(i, stones, visited);
+            parent[i] = i;
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (stones[i][0] == stones[j][0] ||
+                    stones[i][1] == stones[j][1]) {
+
+                    if (union(parent, i, j)) {
+                        components--;
+                    }
+                }
             }
         }
 
         return n - components;
     }
 
-    void dfs(int i, int[][] stones, boolean[] visited) {
-        visited[i] = true;
-
-        for (int j = 0; j < stones.length; j++) {
-            // Connect if same row or same column
-            if (!visited[j] &&
-                (stones[i][0] == stones[j][0] ||
-                 stones[i][1] == stones[j][1])) {
-
-                dfs(j, stones, visited);
-            }
+    private int find(int[] parent, int x) {
+        while (x != parent[x]) {
+            parent[x] = parent[parent[x]];
+            x = parent[x];
         }
+        return x;
+    }
+
+    private boolean union(int[] parent, int a, int b) {
+        int pa = find(parent, a);
+        int pb = find(parent, b);
+
+        if (pa == pb) {
+            return false;
+        }
+
+        parent[pb] = pa;
+        return true;
     }
 }
