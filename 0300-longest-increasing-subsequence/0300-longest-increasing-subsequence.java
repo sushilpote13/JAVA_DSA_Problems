a30e24b7da12c35f2989a81979e5c5626b7ba1df
Sushil Ashok Pote <166101754+sushilpote13@users.noreply.github.com>
2026-09-05 09:35:52 +0530

    Time: 38 ms (63.83%), Space: 46 MB (83.2%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -2,39 +2,32 @@ class Solution {
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
 
-        // Create a set with no duplicate values
-        Set<Integer> set = new HashSet<>();
+        // dp[i] = LIS ending at index i
+        int[] dp = new int[n];
 
+        // Every element itself is a subsequence of length 1
+        Arrays.fill(dp, 1);
+
+        // Check every element
         for (int i = 0; i < n; i++) {
-            set.add(nums[i]);
-        }
 
-        // Convert set to array
-        Integer[] nums2 = set.toArray(new Integer[0]);
+            // Check all previous elements
+            for (int j = 0; j < i; j++) {
 
-        // Sort the array
-        Arrays.sort(nums2);
-
-        // Length of unique array
-        int m = nums2.length;
-
-        // Create DP table for LCS
-        int[][] dp = new int[n + 1][m + 1];
-
-        // Fill the DP table
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                // If elements match
-                if (nums[i - 1] == nums2[j - 1]) {
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                } else {
-                    // Take the maximum
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                // If current element is greater
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
             }
         }
 
-        // Return the LCS length
-        return dp[n][m];
+        // Find the maximum LIS
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            ans = Math.max(ans, dp[i]);
+        }
+
+        return ans;
     }
 }
