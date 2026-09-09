df491f057c9dd615f15ef86a4d9e010c77419995
Sushil Ashok Pote <166101754+sushilpote13@users.noreply.github.com>
2026-09-09 23:10:07 +0530

    Time: 8 ms (73.84%), Space: 65.5 MB (24.19%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,42 +1,61 @@
 class Solution {
-    public static boolean checkPalindrome(String s) {
-        int st = 0;
-        int en = s.length() - 1;
-        while (st <= en) {
-            if (s.charAt(st) != s.charAt(en)) {
-                return false;
-            }
-            st++;
-            en--;
-        }
-        return true;
-    }
 
-    public static void recursive(List<List<String>> result, List<String> current, String s, int start) {
-        // base condition 
+    public void recursive(
+        List<List<String>> result,
+        List<String> current,
+        String s,
+        int start,
+        boolean[][] dp
+    ) {
+        // Base condition
         if (start == s.length()) {
             result.add(new ArrayList<>(current));
             return;
         }
 
         for (int i = start; i < s.length(); i++) {
-            String substring = s.substring(start, i + 1);
-            if (checkPalindrome(substring)) {
+
+            // s[start...i] is palindrome
+            if (dp[start][i]) {
+
+                String substring = s.substring(start, i + 1);
+
                 current.add(substring);
 
-                // recursion code 
-                recursive(result, current, s, i + 1);
-                
-                // backtaring
+                // Move to next part
+                recursive(result, current, s, i + 1, dp);
+
+                // Backtracking
                 current.remove(current.size() - 1);
             }
         }
     }
 
     public List<List<String>> partition(String s) {
+
+        int n = s.length();
+
+        // dp[i][j] = true if s[i...j] is palindrome
+        boolean[][] dp = new boolean[n][n];
+
+        // Build palindrome DP
+        for (int i = n - 1; i >= 0; i--) {
+
+            for (int j = i; j < n; j++) {
+
+                if (s.charAt(i) == s.charAt(j) &&
+                    (j - i <= 2 || dp[i + 1][j - 1])) {
+
+                    dp[i][j] = true;
+                }
+            }
+        }
+
         List<List<String>> result = new ArrayList<>();
         List<String> current = new ArrayList<>();
-        recursive(result, current, s, 0);
+
+        recursive(result, current, s, 0, dp);
+
         return result;
     }
 }
